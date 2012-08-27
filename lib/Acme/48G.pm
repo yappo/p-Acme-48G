5fc561e18a294db4d255858cea92b69a12982858
Kazuhiro Osawa <yappo@shibuya.pl>
2012-08-27 17:24:45 +0900

    initial commit

---
+++ lib/Acme/48G.pm
@@ -0,0 +1,32 @@
+package Acme::48G;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::48G -
+
+=head1 SYNOPSIS
+
+    use Acme::48G;
+
+=head1 DESCRIPTION
+
+Acme::48G is
+
+=head1 AUTHOR
+
+Kazuhiro Osawa E<lt>yappo {at} shibuya {dot} plE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
