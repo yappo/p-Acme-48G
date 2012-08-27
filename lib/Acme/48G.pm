7c214159d60c47b1530c1a09902d4ad0528d2ce2
Kazuhiro Osawa <yappo@shibuya.pl>
2012-08-27 17:40:19 +0900

    impliment

--- lib/Acme/48G.pm
+++ lib/Acme/48G.pm
@@ -2,21 +2,39 @@ package Acme::48G;
 use strict;
 use warnings;
 our $VERSION = '0.01';
+require Acme::Acotie;
+
+*sokaku = *Acme::Acotie::import;
 
 1;
 __END__
 
 =head1 NAME
 
-Acme::48G -
+Acme::48G - your module
 
 =head1 SYNOPSIS
 
+    use strict;
+    use warnings;
     use Acme::48G;
+    
+    sub AikaTada      { print "らぶたん\n" }
+    sub AkiTakajyo     { print "あきちゃ\n" }
+    sub HarukaNakagawa { print "元はるごん\n" }
+    sub SaeMiyazawa    { print "さえちゃん\n" }
+    sub MariyaSuzuki   { print "まりやんぬ\n" }
+    sub AnnaIshida     { print "あんにゃ\n" }
+    sub RieKitahara    { print "きたえりえ\n" }
+    sub YuiYokoyama    { print "ゆいはん\n" }
+
+    AkiTakajyo;
+    Acme::48G->sokaku;
+    AkiTakajyo; # 組閣ed
 
 =head1 DESCRIPTION
 
-Acme::48G is
+Acme::48G is your module.
 
 =head1 AUTHOR
 
